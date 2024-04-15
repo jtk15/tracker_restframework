e49ece8570aa49c1a526bb3b4083c94595a013f5
jtk15 <joeltonportopl15@gmail.com>
2024-04-15 01:13:18 -0300

    Foi criado o modelo de cidade.
    close #8

--- core/models.py
+++ core/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
-class State(models.Model):
+
+class Base(models.Model):
     name = models.CharField('Estado', max_length=30, unique=True)
+
+
+class State(Base):
     abbreviation = models.CharField('Sigla', max_length=2, unique=True)
+
+
+class City(Base):
+    state_fk = models.ForeignKey(State, related_name="cities", on_delete=models.CASCADE)
